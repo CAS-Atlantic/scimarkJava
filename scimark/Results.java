5d940cc7c48ddc7cc067583f38a37d43431aa764
Aaron Graham <aaron.graham@unb.ca>
2021-02-15 23:31:25 -0400

    Update Author Information
    Signed-off-by: Aaron Graham <aaron.graham@unb.ca>

--- scimark/Results.java
+++ scimark/Results.java
@@ -1,3 +1,19 @@
+/**
+ *  SciMark2: A Java numerical benchmark measuring performance
+ *  of computational kernels for FFTs, Monte Carlo simulation,
+ *  sparse matrix computations, Jacobi SOR, and dense LU matrix
+ *  factorizations.
+ *
+ *  Authors: Roldan Pozo (pozo@nist.gov) and Bruce Miller (bruce.miller@nist.gov)
+ *
+ *  Downloaded from: https://math.nist.gov/scimark2/download.html
+ *
+ *  Modified by: Aaron Graham (aaron.graham@unb.ca, aarongraham9@gmail.com) and
+ *                Jean-Philippe Legault (jlegault@unb.ca, jeanphilippe.legault@gmail.com)
+ *                 for the Centre for Advanced Studies - Atlantic (CAS-Atlantic) at the
+ *                  Univerity of New Brunswick in Fredericton, New Brunswick, Canada
+*/
+
 package scimark;
 
 public class Results
